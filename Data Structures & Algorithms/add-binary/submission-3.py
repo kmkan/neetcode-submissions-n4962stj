2a493cc5e92bcb8f14489603f0ffdd5772e6de99
kmkan <kandpalkbusiness@gmail.com>
2026-03-31 08:11:24 -0600

    Add: add-binary - submission-3

---
+++ Data Structures & Algorithms/add-binary/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        x, y = int(a, 2), int(b, 2)
+
+        
+        while y:
+            ans = x ^ y
+
+            carry = (x & y) << 1
+
+            x, y = ans, carry
+        
+        return bin(x)[2:]
