118b3dbce3125c96b40e7b172624d815038246f8
kmkan <kandpalkbusiness@gmail.com>
2026-05-13 20:21:24 -0600

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        prev2 = 1
+        prev = 1
+
+        for i in range(2, n + 1):
+            cur_i = prev2 + prev
+            prev2, prev = prev, cur_i
+        
+        return prev
